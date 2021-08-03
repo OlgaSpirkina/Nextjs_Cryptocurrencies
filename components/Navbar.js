086c2077f749c6ee91cdc06ddbd8225fbf15f8a4
olga <>
2021-08-03 16:57:00 +0200

    create web app with next.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,36 @@
 import React from 'react'
-import Broad from './Broad'
-import fetch from "node-fetch";
 import styles from './Navbar.module.css'
-export default function Navbar({broadInfo}) {
-  return(
+
+function Navbar({ broadInfo }) {
+  return (
     <nav className={styles.main_style}>
-      <div className={styles.flex}>
-        <a href="/" className="rounded bg-blue-500 ">Coins</a>
+      <div className={styles.logo_container}>
+        <a href="https://www.shayp.com/fr/">
+          <img className={styles.logoimg} src="http://i1.wp.com/www.shayp.com/wp-content/uploads/2021/07/cropped-cropped-websitelogooooo-1-1.png?fit=100%2C51&ssl=1" alt="shayp logo" />
+        </a>
+        <span><strong>Shayp </strong><br/>The future of cryptocurrency</span>
       </div>
-      <div className={styles.flex}>
-        <a href="/converter" className="rounded bg-blue-500 ">Converter</a>
-      </div>
+      <ul id={styles.navigation}>
+        <li>
+          <a href="/">Coins</a>
+        </li>
+        <li>
+          <a href="/converter">Converter</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+      </ul>
       <div>
-        <Broad broadInfo={broadInfo}/>
+        <ul id={styles.broad_info}>Some info
+          <li>BTC Dominance: <span>{broadInfo.bitcoin_dominance_percentage} %</span></li>
+          <li>Vol 24h: <span>{broadInfo.volume_24h_usd}</span></li>
+          <li>Market Cap: <span>{broadInfo.market_cap_ath_value}</span></li>
+          <li>Total quantity: <span>{broadInfo.cryptocurrencies_number}</span></li>
+        </ul>
       </div>
     </nav>
   )
 }
+
+export default Navbar
--- pages/converter.js
+++ pages/converter.js
@@ -1,11 +1,20 @@
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
-export default function Converter(){
+export default function Converter({broadInfo}){
   return (
-  <div className={styles.main_style}>
-    <Navbar />
+  <div className={styles.main_container}>
+    <Navbar broadInfo={broadInfo}/>
     <h1>Welcome to Converter !</h1>
   </div>
   )
+}
+export async function getStaticProps() {
+  const res = await fetch('https://api.coinpaprika.com/v1/global')
+  const broadInfo = await res.json()
+  return {
+    props: {
+      broadInfo
+    },
+  }
 }
