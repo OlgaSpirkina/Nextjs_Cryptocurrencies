eede2fa8a77b00c3092263e36e0b161cb6346d73
olga <>
2021-08-07 14:16:11 +0200

    keep css styling; create new component for global info; make navbar part of layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
-import NavbarLayout from './NavbarLayout'
+import Head from 'next/head'
+import Navbar from './Navbar'
 import styles from '../styles/Layout.module.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -6,12 +7,20 @@ import {
   faLinkedin
 } from '@fortawesome/free-brands-svg-icons';
 import{faEnvelopeOpen} from '@fortawesome/free-regular-svg-icons';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
 
-export default function Layout({ children }){
+export default function Layout({ children, broadInfo }){
   return (
     <div className={styles.container}>
-      <NavbarLayout />
-        {children}
+      <Head>
+        <title>Shayp Coins</title>
+        <meta name="description" content="Criptocurrency display web application" />
+        <link rel="icon" href="/logoshayp.png" />
+      </Head>
+      <Navbar />
+        {children }
       <footer id={styles.footer}>
         <div id={styles.copyright}>Olga Spirkina &copy; 2021</div>
         <div id={styles.socialmedia}>
--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import styles from './Navbar.module.css'
 
-function Navbar({ percentage, volume_24h_usd, market_cap, cryptocurrencies_number }) {
+function Navbar() {
   return (
-      <div>
-        <ul id={styles.broad_info}>Some info
-          <li>BTC Dominance: <span>{percentage} %</span></li>
-          <li>Vol 24h: <span>{volume_24h_usd}</span></li>
-          <li>Market Cap: <span>{}</span></li>
-          <li>Total quantity: <span>{cryptocurrencies_number}</span></li>
-        </ul>
+    <nav className={styles.main_style}>
+      <div className={styles.logo_container}>
+        <a href="https://www.shayp.com/fr/">
+          <img className={styles.logoimg} src="http://i1.wp.com/www.shayp.com/wp-content/uploads/2021/07/cropped-cropped-websitelogooooo-1-1.png?fit=100%2C51&ssl=1" alt="shayp logo" />
+        </a>
+        <span><strong>Shayp </strong><br/>The future of cryptocurrency</span>
       </div>
+      <ul id={styles.navigation}>
+        <li>
+          <a href="/">Coins</a>
+        </li>
+        <li>
+          <a href="/converter">Converter</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
