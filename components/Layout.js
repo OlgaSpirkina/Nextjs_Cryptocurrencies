c195455458dd04be58a3b1bd24e9269ea56a2862
olga <>
2021-08-07 12:48:33 +0200

    work on Navbar

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import NavbarLayout from './NavbarLayout'
 import styles from '../styles/Layout.module.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -6,10 +7,10 @@ import {
 } from '@fortawesome/free-brands-svg-icons';
 import{faEnvelopeOpen} from '@fortawesome/free-regular-svg-icons';
 
-
-export default function Layout({children}){
+export default function Layout({ children }){
   return (
     <div className={styles.container}>
+      <NavbarLayout />
         {children}
       <footer id={styles.footer}>
         <div id={styles.copyright}>Olga Spirkina &copy; 2021</div>
@@ -21,5 +22,4 @@ export default function Layout({children}){
       </footer>
     </div>
   )
-
 }
